9b0d523a1b6a8abcb219f47ff61a54dd28405714
hifishch <56207177+hifishch@users.noreply.github.com>
2019-11-09 11:11:19 +0100

    Intial commit: Version 1.0.0

---
+++ CheckAndRestart-VM.ps1
@@ -0,0 +1,49 @@
+﻿#Powershell HyperV VM-Check for Solarwinds RMM
+#Author: Andreas Walker andreas.walker@walkerit.ch
+#Licence: GNU General Public License v3.0
+#Version: 1.0.0 / 20.10.2019
+
+#Define paremeter
+Param (
+[string]$VM,
+$Start = 'no'
+)
+
+#Get status of selected VM
+$VMStatus = Get-VM | where {$_.Name -eq $VM}
+
+#Check Status of selected VM
+if ($VMStatus.State -like "Running")
+    {
+    Write-Host "OK - VM $VM is running for"$VMStatus.Uptime
+    Exit 0
+    }
+    else
+        {
+        Write-Host "Error - VM $VM is not running"
+        if ($Start -eq 'no')
+            {
+            Exit 1001
+            }
+            else
+                {
+                Write-Host "Trying to Start VM $VM"
+                Start-VM -Name $VM
+                Start-Sleep -Seconds 60
+                $NewVMStatus = Get-VM | where {$_.Name -eq $VM}
+                if ($VMStatus.State -like "Running")
+                    {
+                    Write-Host "VM $VM is running now"
+                    Exit 0
+                    }
+                    else
+                        {
+                        Write-Host "VM $VM is still not running"
+                        Exit 1001
+                        }
+                }
+        }
+
+#Catch unexpected end of Script
+Write-Host "FAILURE - Script ended unexpected"
+Exit 1001
